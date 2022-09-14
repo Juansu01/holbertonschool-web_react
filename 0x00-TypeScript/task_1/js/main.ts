4707a6cedc1060ec470fe1fd577ea0ea19ab6d9e
Juansu01 <thejuank0@hotmail.com>
2022-09-14 08:37:32 -0500

    Add: Printing teachers

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -46,3 +46,14 @@ console.log(director1);
 // lastName: "Doe"
 // location: "London"
 // numberOfReports: 17
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = function (firstName: string, lastName: string):
+  string { return `${firstName.charAt(0)}. ${lastName}`}
+
+console.log(printTeacher("John", "Doe"))
+
+// printTeacher("John", "Doe") -> J. Doe
