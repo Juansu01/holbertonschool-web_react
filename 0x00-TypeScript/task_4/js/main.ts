cd1f40460e172589dfdf80cc599889038c2c4591
Juansu01 <thejuank0@hotmail.com>
2022-09-14 13:20:05 -0500

    Add: Namespace & Declaration merging

--- 0x00-TypeScript/task_4/js/main.ts
+++ 0x00-TypeScript/task_4/js/main.ts
@@ -1,45 +1,30 @@
-interface Student {
-    firstName: string;
-    lastName: string;
-    age: number;
-    location: string;
+/// <reference path="./subjects/Cpp.ts" />
+/// <reference path="./subjects/Java.ts" />
+/// <reference path="./subjects/React.ts" />
+/// <reference path="./subjects/Subject.ts" />
+/// <reference path="./subjects/Teacher.ts" />
+
+export const cpp: Subjects.Cpp = new Subjects.Cpp();
+export const java: Subjects.Java = new Subjects.Java();
+export const react: Subjects.React = new Subjects.React();
+
+export const cTeacher: Subjects.Teacher = {
+  firstName: "Guillaume",
+  lastName: "Salva",
+  experienceTeachingC: 10,
 }
 
-const student1: Student = {
-    firstName: "Jennie",
-    lastName: "Kim",
-    age: 26,
-    location: "Seoul",
-}
+console.log("C++")
+cpp.setTeacher = cTeacher
+console.log(cpp.getRequirements())
+console.log(cpp.getAvailableTeacher())
 
-const student2: Student = {
-    firstName: "Lalisa",
-    lastName: "Manobal",
-    age: 25,
-    location: "Seoul",
-}
+console.log("Java")
+java.setTeacher = cTeacher
+console.log(java.getRequirements())
+console.log(java.getAvailableTeacher())
 
-const studentsList: Array<Student> = [student1, student2]
-const body: HTMLBodyElement = document.getElementsByTagName("body")[0]
-const table: HTMLTableElement = document.createElement("table")
-const thead: HTMLTableSectionElement = document.createElement("thead")
-const tbody: HTMLTableSectionElement = document.createElement("tbody")
-const rowHead: HTMLTableRowElement = thead.insertRow(0)
-const cell1Head: HTMLTableCellElement = rowHead.insertCell(0)
-cell1Head.innerHTML = "firstName"
-const cell2Head: HTMLTableCellElement = rowHead.insertCell(1)
-cell2Head.innerHTML = "location"
-
-table.append(thead)
-
-studentsList.forEach((student) => {
-    const row: HTMLTableRowElement = tbody.insertRow(0)
-    const firstCell: HTMLTableCellElement = row.insertCell(0)
-    const secondCell: HTMLTableCellElement = row.insertCell(1);
-
-    firstCell.innerHTML = student.firstName
-    secondCell.innerHTML = student.location
-})
-
-table.append(tbody)
-body.append(table)
+console.log("React")
+react.setTeacher = cTeacher
+console.log(react.getRequirements())
+console.log(react.getAvailableTeacher())
