6370c565c9e7bb8f9b34124224c4c8ae59c4c646
Juansu01 <thejuank0@hotmail.com>
2022-09-14 08:25:28 -0500

    Add: Let's build a Teacher interface

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -1,45 +1,26 @@
-interface Student {
-    firstName: string;
-    lastName: string;
-    age: number;
-    location: string;
+interface Teacher {
+  firstName: string;
+  lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  [key: string]: any;
+
 }
 
-const student1: Student = {
-    firstName: "Jennie",
-    lastName: "Kim",
-    age: 26,
-    location: "Seoul",
-}
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
 
-const student2: Student = {
-    firstName: "Lalisa",
-    lastName: "Manobal",
-    age: 25,
-    location: "Seoul",
-}
+console.log(teacher3);
 
-const studentsList: Array<Student> = [student1, student2]
-const body: HTMLBodyElement = document.getElementsByTagName("body")[0]
-const table: HTMLTableElement = document.createElement("table")
-const thead: HTMLTableSectionElement = document.createElement("thead")
-const tbody: HTMLTableSectionElement = document.createElement("tbody")
-const rowHead: HTMLTableRowElement = thead.insertRow(0)
-const cell1Head: HTMLTableCellElement = rowHead.insertCell(0)
-cell1Head.innerHTML = "firstName"
-const cell2Head: HTMLTableCellElement = rowHead.insertCell(1)
-cell2Head.innerHTML = "location"
-
-table.append(thead)
-
-studentsList.forEach((student) => {
-    const row: HTMLTableRowElement = tbody.insertRow(0)
-    const firstCell: HTMLTableCellElement = row.insertCell(0)
-    const secondCell: HTMLTableCellElement = row.insertCell(1);
-
-    firstCell.innerHTML = student.firstName
-    secondCell.innerHTML = student.location
-})
-
-table.append(tbody)
-body.append(table)
+// should print
+// Object
+// contract: false
+// firstName: "John"
+// fullTimeEmployee: false
+// lastName: "Doe"
+// location: "London"
