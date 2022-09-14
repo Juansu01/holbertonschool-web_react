622c4a0b915a3ce6c132e98bf5ab4b803aceecd1
Juansu01 <thejuank0@hotmail.com>
2022-09-14 13:11:23 -0500

    Add: Ambient Namespaces

--- 0x00-TypeScript/task_3/js/main.ts
+++ 0x00-TypeScript/task_3/js/main.ts
@@ -1,45 +1,12 @@
-interface Student {
-    firstName: string;
-    lastName: string;
-    age: number;
-    location: string;
-}
+import { RowID, RowElement } from "./interface";
+import * as CRUD from "./crud";
 
-const student1: Student = {
-    firstName: "Jennie",
-    lastName: "Kim",
-    age: 26,
-    location: "Seoul",
-}
+const row: RowElement = {
+  firstName: "Guillaume",
+  lastName: "Salva",
+};
 
-const student2: Student = {
-    firstName: "Lalisa",
-    lastName: "Manobal",
-    age: 25,
-    location: "Seoul",
-}
-
-const studentsList: Array<Student> = [student1, student2]
-const body: HTMLBodyElement = document.getElementsByTagName("body")[0]
-const table: HTMLTableElement = document.createElement("table")
-const thead: HTMLTableSectionElement = document.createElement("thead")
-const tbody: HTMLTableSectionElement = document.createElement("tbody")
-const rowHead: HTMLTableRowElement = thead.insertRow(0)
-const cell1Head: HTMLTableCellElement = rowHead.insertCell(0)
-cell1Head.innerHTML = "firstName"
-const cell2Head: HTMLTableCellElement = rowHead.insertCell(1)
-cell2Head.innerHTML = "location"
-
-table.append(thead)
-
-studentsList.forEach((student) => {
-    const row: HTMLTableRowElement = tbody.insertRow(0)
-    const firstCell: HTMLTableCellElement = row.insertCell(0)
-    const secondCell: HTMLTableCellElement = row.insertCell(1);
-
-    firstCell.innerHTML = student.firstName
-    secondCell.innerHTML = student.location
-})
-
-table.append(tbody)
-body.append(table)
+const newRowID: RowID = CRUD.insertRow(row);
+const updatedRow: RowElement = { ...row, age: 23 };
+CRUD.updateRow(newRowID, updatedRow);
+CRUD.deleteRow(newRowID);
