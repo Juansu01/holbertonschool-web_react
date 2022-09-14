6a1f66dfee9d531d3a0f40388f3040e02e1d9237
Juansu01 <thejuank0@hotmail.com>
2022-09-14 09:22:52 -0500

    Add: Advanced types Part 1

--- 0x00-TypeScript/task_2/js/main.ts
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -1,45 +1,52 @@
-interface Student {
-    firstName: string;
-    lastName: string;
-    age: number;
-    location: string;
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
 }
 
-const student1: Student = {
-    firstName: "Jennie",
-    lastName: "Kim",
-    age: 26,
-    location: "Seoul",
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
 }
 
-const student2: Student = {
-    firstName: "Lalisa",
-    lastName: "Manobal",
-    age: 25,
-    location: "Seoul",
+class Director implements DirectorInterface {
+  workFromHome(): string {
+    console.log("Working from home")
+    return "Working from home"
+  }
+
+  getCoffeeBreak(): string {
+    console.log("Getting a coffee break")
+    return "Getting a coffee break"
+  }
+
+  workDirectorTasks(): string {
+    console.log("Getting to director tasks")
+    return "Getting to director tasks"
+  }
 }
 
-const studentsList: Array<Student> = [student1, student2]
-const body: HTMLBodyElement = document.getElementsByTagName("body")[0]
-const table: HTMLTableElement = document.createElement("table")
-const thead: HTMLTableSectionElement = document.createElement("thead")
-const tbody: HTMLTableSectionElement = document.createElement("tbody")
-const rowHead: HTMLTableRowElement = thead.insertRow(0)
-const cell1Head: HTMLTableCellElement = rowHead.insertCell(0)
-cell1Head.innerHTML = "firstName"
-const cell2Head: HTMLTableCellElement = rowHead.insertCell(1)
-cell2Head.innerHTML = "location"
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return "Cannot work from home";
+  }
+  getCoffeeBreak(): string {
+    return "Cannot have a break";
+  }
+  workTeacherTasks(): string {
+    return "Getting to work";
+  }
+}
 
-table.append(thead)
+const createEmployee = (salary: number | string): Teacher | Director => {
+  if (salary < 500) {
+    return new Teacher();
+  }
 
-studentsList.forEach((student) => {
-    const row: HTMLTableRowElement = tbody.insertRow(0)
-    const firstCell: HTMLTableCellElement = row.insertCell(0)
-    const secondCell: HTMLTableCellElement = row.insertCell(1);
+  return new Director();
+}
 
-    firstCell.innerHTML = student.firstName
-    secondCell.innerHTML = student.location
-})
-
-table.append(tbody)
-body.append(table)
+console.log(createEmployee(200));
+console.log(createEmployee(1000));
+console.log(createEmployee("$500"));
