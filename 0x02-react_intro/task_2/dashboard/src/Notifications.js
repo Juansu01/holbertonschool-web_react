a5997bfae5b07105d0c4cfd736629069c100db4d
Juansu01 <thejuank0@hotmail.com>
2022-09-21 16:34:47 -0500

    Add: Modify the Notifications

--- 0x02-react_intro/task_2/dashboard/src/Notifications.js
+++ 0x02-react_intro/task_2/dashboard/src/Notifications.js
@@ -1,9 +1,34 @@
 import React from "react";
+import closeIcon from "./close-icon.png";
+import { getLatestNotification } from "./utils";
+import "./Notifications.css";
+
+const buttonStyle = {
+  background: "transparent",
+  border: "none",
+  position: "absolute",
+  right: 20,
+}
+
+function logToConsole() {
+  console.log("Close button has been clicked")
+}
 
 function Notifications() {
     return (
     <div className="Notifications">
+      <button onClick={logToConsole} style={buttonStyle}>
+        <img src={closeIcon} alt="closeIcon" width="10" height="10"></img>
+      </button>
       <p>Here is the list of notifications</p>
+      <ul>
+        <li data-priority="default">New course available</li>
+        <li data-priority="urgent">New resume available</li>
+        <li
+          data-priority="urgent"
+          dangerouslySetInnerHTML={{ __html: getLatestNotification() }}
+        ></li>
+      </ul>
     </div>
     );
 };
