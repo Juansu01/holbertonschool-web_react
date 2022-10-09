dd598d98d4041d56cdd86a4f50277236ef75fa25
Juansu01 <thejuank0@hotmail.com>
2022-10-09 14:15:03 -0500

    Add: Shapes, Loops, and Keys

---
+++ 0x03-react_props/task_5/dashboard/src/App/App.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./App.css";
+import { getLatestNotification } from "../utils/utils";
+import Notifications from "../Notifications/Notifications";
+import Header from "../Header/Header";
+import Login from "../Login/Login";
+import Footer from "../Footer/Footer";
+import CourseList from "../CourseList/CourseList";
+import PropTypes from "prop-types";
+
+
+const listCourses = [
+  { id: 1, name: "ES6", credit: 60 },
+  { id: 2, name: "Webpack", credit: 20 },
+  { id: 3, name: "React", credit: 40 },
+];
+
+const listNotifications = [
+  { id: 1, type: "default", value: "New course available" },
+  { id: 2, type: "default", value: "New resume available" },
+  { id: 3, type: "urgent", html: { __html: getLatestNotification() } },
+];
+
+function App({isLoggedIn}) {
+  return (
+    <>
+      <Notifications displayDrawer listNotifications={listNotifications} />
+      <div className="App">
+        <Header />
+      </div>
+      <div className="App-body">{!isLoggedIn ? <Login /> : <CourseList listCourses={listCourses} />}</div>
+        <Footer />
+  </>
+  );
+}
+
+App.defaultProps = {
+  isLoggedIn: false,
+}
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+}
+
+export default App;
---
+++ 0x03-react_props/task_5/dashboard/src/App/App.test.js
@@ -0,0 +1,41 @@
+import { shallow } from "enzyme";
+import React from "react";
+import App from "./App";
+
+describe("<App />", () => {
+  it("App renders without crashing", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.exists()).toEqual(true);
+  });
+  it("should contain the Notifications component", () => {
+    const wrapper = shallow(<App />);
+    wrapper.update();
+    expect(wrapper.find("Notifications")).toHaveLength(1);
+  });
+  it("should contain the Header component", () => {
+    const wrapper = shallow(<App />);
+    wrapper.update();
+    expect(wrapper.find("Header")).toHaveLength(1);
+  });
+  it("should contain the Login component", () => {
+    const wrapper = shallow(<App />);
+    wrapper.update();
+    expect(wrapper.find("Login")).toHaveLength(1);
+  });
+  it("should contain the Footer component", () => {
+    const wrapper = shallow(<App />);
+    wrapper.update();
+    expect(wrapper.find("Footer")).toHaveLength(1);
+  });
+  it("CourseList is not displayed when isLoggedIn is false", () => {
+    const wrapper = shallow(<App />);
+    wrapper.update();
+    expect(wrapper.find("CourseList")).toHaveLength(0);
+  })
+  it("Diplays CourseList when isLoggedIn is true", () => {
+    const wrapper = shallow(<App isLoggedIn />);
+    wrapper.update();
+    expect(wrapper.find("Login")).toHaveLength(0);
+    expect(wrapper.find("CourseList")).toHaveLength(1);
+  });
+});
