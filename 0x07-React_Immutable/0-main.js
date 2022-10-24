98cd77d932993d6f68c3fbf6d806942c2fdc7cc2
Juansu01 <thejuank0@hotmail.com>
2022-10-24 16:23:42 -0500

    Add: Converting into an Immutable object using fromJS

---
+++ 0x07-React_Immutable/0-main.js
@@ -0,0 +1,11 @@
+import getImmutableObject from './0-fromjs.js';
+
+
+const myObject = {
+    fear: true,
+    smell: -1033575916.9145899,
+    wall: false,
+    thing: -914767132
+}
+
+console.log(getImmutableObject(myObject));
