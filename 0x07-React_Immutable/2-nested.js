9cc7834d88f0fa99cdd4e740d48d86cb63d615cc
Juansu01 <thejuank0@hotmail.com>
2022-10-25 16:45:26 -0500

    Using fromJS

--- 0x07-React_Immutable/2-nested.js
+++ 0x07-React_Immutable/2-nested.js
@@ -1,7 +1,7 @@
-import { Map } from 'immutable';
+import { fromJS } from 'immutable';
 
 export default function accessImmutableObject(object, array) {
-  const toMap = Map(object);
+  const toMap = fromJS(object);
   const value = toMap.getIn(array, undefined);
   return value;
 }
