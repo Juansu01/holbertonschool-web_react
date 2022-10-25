1a772beefb27683fb7d4f2b2663535f39991a567
Juansu01 <thejuank0@hotmail.com>
2022-10-25 16:37:10 -0500

    Test indent

--- 0x07-React_Immutable/0-fromjs.js
+++ 0x07-React_Immutable/0-fromjs.js
@@ -1,6 +1,6 @@
 import { fromJS } from 'immutable';
 
 export default function getImmutableObject(object) {
-  const immutableObject = fromJS(object);
-  return immutableObject;
+    const immutableObject = fromJS(object);
+    return immutableObject;
 }
