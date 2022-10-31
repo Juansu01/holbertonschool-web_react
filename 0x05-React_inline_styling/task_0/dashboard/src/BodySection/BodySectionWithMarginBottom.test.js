a70acc48d7453d1ea659c500e6c1acef3ac5e458
Juansu01 <thejuank0@hotmail.com>
2022-10-31 17:27:32 -0500

    Add: Inline styling

---
+++ 0x05-React_inline_styling/task_0/dashboard/src/BodySection/BodySectionWithMarginBottom.test.js
@@ -0,0 +1,27 @@
+import { shallow } from "enzyme";
+import React from "react";
+import BodySectionWithMarginBottom from "./BodySectionWithMarginBottom";
+
+describe("Testing <BodySectionWithMarginBottom />", () => {
+    it("BodySectionWithMarginBottom renders without crashing", () => {
+      const wrapper = shallow(<BodySectionWithMarginBottom />);
+      expect(wrapper.exists()).toEqual(true);
+    });
+    it("Component should render correctly the children and one h2 element", () => {
+    const wrapper = shallow(<BodySectionWithMarginBottom title="test title"><p>test children node</p></BodySectionWithMarginBottom>);
+    const BodySectionComponent = wrapper.find("BodySection");
+
+    expect(BodySectionComponent).toHaveLength(1);
+    expect(BodySectionComponent.props().title).toEqual("test title");
+
+    const BodySectionChildren = BodySectionComponent.dive();
+
+    const h2Tag = BodySectionChildren.find("h2");
+    const pTag = BodySectionChildren.find("p");
+
+    expect(h2Tag).toHaveLength(1);
+    expect(h2Tag.text()).toEqual("test title");
+    expect(pTag).toHaveLength(1);
+    expect(pTag.text()).toEqual("test children node");
+    });
+});
---
+++ 0x05-React_inline_styling/task_0/dashboard/src/HOC/WithLogging.test.js
@@ -0,0 +1,35 @@
+import { mount } from "enzyme";
+import React from "react";
+import withLogging from "./WithLogging";
+import Login from "../Login/Login";
+
+describe("Testing <WithLogging />", () => {
+  it("Makes sure console.log is called when mounting and unmounting using pure html", () => {
+
+    console.log = jest.fn();
+    const HOC = withLogging(() => <p />);
+
+    const wrapper = mount(<HOC />);
+    expect(wrapper.exists()).toEqual(true);
+
+    expect(console.log).toHaveBeenNthCalledWith(1,"Component Component is mounted");
+    wrapper.unmount();
+    expect(console.log).toHaveBeenNthCalledWith(2,"Component Component is going to unmount");
+
+    jest.restoreAllMocks();
+  });
+  it("Makes sure console.log is called with the name of the component when mounting and unmounting", () => {
+
+    console.log = jest.fn();
+    const HOC = withLogging(Login);
+
+    const wrapper = mount(<HOC />);
+    expect(wrapper.exists()).toEqual(true);
+
+    expect(console.log).toHaveBeenNthCalledWith(1,"Component Login is mounted");
+    wrapper.unmount();
+    expect(console.log).toHaveBeenNthCalledWith(2,`Component Login is going to unmount`);
+
+    jest.restoreAllMocks();
+  });
+});
