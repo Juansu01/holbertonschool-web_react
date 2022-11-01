1f7811452d4d883e3c5b6cdea386071790a35f43
Juansu01 <thejuank0@hotmail.com>
2022-11-01 15:00:19 -0500

    Add: Conditionally applying style

---
+++ 0x05-React_inline_styling/task_2/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,61 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { StyleSheet, css } from "aphrodite";
+
+const NotificationItem = React.memo(function NotificationItem({
+  type, value, html, markAsRead, id}) {
+
+  let listItem;
+
+  let styleType = css(type === "urgent" ? styles.urgent : styles.default);
+
+  if (value) {
+    listItem = (
+      <li className={styleType} data-notification-type={type} onClick={() => markAsRead(id)}>
+        {value}
+      </li>
+    );
+  } else {
+    listItem = (
+      <li
+        className={styleType}
+        data-notification-type={type}
+        dangerouslySetInnerHTML={html}
+        onClick={() => markAsRead(id)}
+      ></li>
+    );
+  }
+
+  return listItem;
+
+});
+
+const styles = StyleSheet.create({
+  default: {
+    color: "blue",
+  },
+
+  urgent: {
+    color: "red",
+  },
+});
+
+NotificationItem.defaultProps = {
+  type: "default",
+  value: "",
+  html: {},
+  markAsRead: () => {},
+  id: NaN
+};
+
+NotificationItem.propTypes = {
+  type: PropTypes.string,
+  value: PropTypes.string,
+  html: PropTypes.shape({
+    __html: PropTypes.string,
+  }),
+  markAsRead: PropTypes.func,
+  id: PropTypes.number
+};
+
+export default NotificationItem;
